83a63da6e29c00b374d52b54462b4abf1554148c
Jamie Brown <jmbrowncow@gmail.com>
2018-09-21 21:24:52 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -22,3 +22,27 @@ function init(e) {
 const body = document.body;
 
 body.addEventListener('keydown', init);
+
+
+
+// function init() {
+//   let index = 0;
+// 
+//   document.body.addEventListener('keydown', function(e) {
+//     const key = parseInt(e.which || e.detail);
+// 
+//     if (code[index] === key) {
+//       index++;
+// 
+//       if (index === code.length) {
+//         alert('Hurray!');
+// 
+//         index = 0;
+//       }
+//     } else {
+//       index = 0;
+//     }
+//   });
+// }
+// 
+// init();
